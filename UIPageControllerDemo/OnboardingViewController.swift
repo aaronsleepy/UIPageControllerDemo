59ad31e9c4eecb10b334a4261c1352dafd61f5eb
aaronsleepy <aaron@kmong.com>
2022-11-20 17:19:22 +0900

    feat: OnboardingCell과 ViewController 연동

--- UIPageControllerDemo/OnboardingViewController.swift
+++ UIPageControllerDemo/OnboardingViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let messages: [OnboardingMessage] = OnboardingMessage.messages
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.estimatedItemSize = .zero
+        }
     }
-
-
 }
+
+extension OnboardingViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCell", for: indexPath) as? OnboardingCell else {
+            return UICollectionViewCell()
+        }
+        
+        let message = messages[indexPath.item]
+        cell.configiure(message)
+        return cell
+    }
+    
+    
+}
+
+extension OnboardingViewController : UICollectionViewDelegateFlowLayout {
+    
+}
+
 
