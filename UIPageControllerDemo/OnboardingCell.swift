a6aa8445017576b944fab2196c90876650321e44
aaronsleepy <aaron@kmong.com>
2022-11-20 17:13:59 +0900

    feat: Onboarding Cell 추가

--- UIPageControllerDemo/OnboardingCell.swift
+++ UIPageControllerDemo/OnboardingCell.swift
@@ -9,4 +9,19 @@ import UIKit
 
 class OnboardingCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configiure(_ message: OnboardingMessage) {
+        thumbnailImageView.image = UIImage(named: message.imageName)
+        titleLabel.text = message.title
+        descriptionLabel.text = message.title
+    }
+    
+    
+    
+    
+    
+    
 }
